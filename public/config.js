2e82990ad9aa63a5fbcb5aff7396c8c2ef06d878
Apurva Maheshwari <apurvamaheshwari18@gmail.com>
2017-04-14 20:02:37 +0530

    Login and Sign up page added

--- public/config.js
+++ public/config.js
@@ -5,6 +5,11 @@ app.config(['$routeProvider', '$locationProvider',
         templateUrl: '/dictPage/dict.html',
         controller: 'dictCtrl',
         controllerAs: 'dictCtrl',
-      });
+      })
+      .when('/signIn',{
+        templateUrl:'login/login.html',
+        controller: 'loginCtrl',
+        controllerAs:'loginCtrl',
+      })
     $locationProvider.html5Mode(true);
 }]);
---
+++ public/login/login.js
@@ -0,0 +1,3 @@
+app.controller("loginCtrl",["$scope",function(scope){
+
+}]);
