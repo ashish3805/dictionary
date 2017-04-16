4e6c4866774903ef8539810df9bac651d14fa6ec
Ashish Patel <ashish_3805@hotmail.com>
2017-04-16 13:09:48 +0530

    sign out created

--- public/app.js
+++ public/app.js
@@ -1,15 +1,20 @@
-let app = angular.module('app', ['ui.router','user']);
+let app = angular.module('app', ['ui.router', 'user']);
 
-app.controller('baseController', ["$scope", 'userSrv',
-    function ($scope, userSrv) {
-        $scope.user = {};
-        $scope.isAuthed = function () {
-            if (userSrv.isAuthed()) {
-                $scope.user = userSrv.getUser();
-                return true;
-            } else {
-                return false;
-            }
+app.controller('baseController', ["$scope", 'userSrv', function (
+    $scope, userSrv) {
+    $scope.user = {};
+    $scope.isAuthed = function () {
+        if (userSrv.isAuthed()) {
+            $scope.user = userSrv.getUser();
+            return true;
+        } else {
+            return false;
         }
-    }]);
+    }
+
+    $scope.signOut= function(){
+        userSrv.signOut();
+    }
+
+}]);
 
